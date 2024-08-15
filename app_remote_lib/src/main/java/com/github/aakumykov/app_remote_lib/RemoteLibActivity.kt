9606d0a63911f7d9a10a2a3b67b3ff29d6968195
Andrey Kumykov <aakumykov@yandex.ru>
2024-08-15 11:03:22 +0300

    Модуль app_remote_lib и настройки публикации; v0.0.1-alpha

---
+++ app_remote_lib/src/main/java/com/github/aakumykov/app_remote_lib/RemoteLibActivity.kt
@@ -0,0 +1,20 @@
+package com.github.aakumykov.app_remote_lib
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RemoteLibActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_remote_lib)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
